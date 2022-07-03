87b66f8cf8f61b25ab080d2302dac022c82d7a3f
Taras Bondar <strelezzzz@gmail.com>
2022-07-03 23:53:52 +0300

    bug fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-export const Statistics = (title , stats) => {
+export const Statistics = ({ title , stats }) => {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
