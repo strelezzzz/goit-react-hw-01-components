ea4d454b2cb49ebcca4b07088161f455aa6747f3
Taras Bondar <strelezzzz@gmail.com>
2022-07-05 00:17:15 +0300

    update task 03

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,21 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 
 export const FriendList = ({ friends }) => {
-  console.log(friends);
-  return <ul className={css.friendList}>FriendList</ul>;
+  // console.log(friends);
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          isOnline={isOnline}
+          name={name}
+        />
+      ))}
+    </ul>
+  );
 };
 
 FriendList.propTypes = {
