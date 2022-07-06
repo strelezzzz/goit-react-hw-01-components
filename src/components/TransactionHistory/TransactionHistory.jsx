36e39b9f68cdeaf347cb721b8afd4f372f1a7c69
Taras Bondar <strelezzzz@gmail.com>
2022-07-06 17:17:51 +0300

    update profile & transactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -14,21 +14,13 @@ export const TransactionHistory = ({ transactions }) => {
       <tbody>
         {transactions.map(({ id, type, amount, currency }) => (
           <tr key={id} className={css.tr}>
-            <Transaction type={type} amount={amount} currency={currency} />
+            <td className={css.td}>{type}</td>
+            <td className={css.td}>{amount}</td>
+            <td className={css.td}>{currency}</td>
           </tr>
         ))}
       </tbody>
     </table>
-  );
-};
-
-const Transaction = ({ type, amount, currency }) => {
-  return (
-    <>
-      <td className={css.td}>{type}</td>
-      <td className={css.td}>{amount}</td>
-      <td className={css.td}>{currency}</td>
-    </>
   );
 };
 
